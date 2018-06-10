4645ae5e50f40c784dde3e2f80a5cfc452dcba1f
javajava2 <gorzycki@gmail.com>
2018-06-10 09:22:33 +0200

    dodamo czystwego spring boot'a

---
+++ src/main/java/com/Extended_ToDoList/Extended_ToDoList/ExtendedToDoListApplication.java
@@ -0,0 +1,12 @@
+package com.Extended_ToDoList.Extended_ToDoList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtendedToDoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtendedToDoListApplication.class, args);
+	}
+}
